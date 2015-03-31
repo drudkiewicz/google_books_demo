a18cf18a3bb39a1dabd7fe8506a208913221af84
Daria Rudkiewicz <daria.rudkiewicz@stxnext.pl>
2015-03-31 21:47:47 +0200

    Fixing authors attribute

--- js/main_page/book/book_model.js
+++ js/main_page/book/book_model.js
@@ -5,14 +5,13 @@ define(function(require, exports, module) {
 
     return Backbone.Model.extend({
         initialize: function (options) {
-            this.set('authors', options.volumeInfo.authors);
-            if (options.volumeInfo.imageLinks) {
-                this.set('thumbnail', options.volumeInfo.imageLinks.smallThumbnail);
-            } else {
-                this.set('thumbnail', '/images/no_image.jpg');
-            }
-            this.set('title', options.volumeInfo.title);
-            this.set('previewLink', options.volumeInfo.previewLink);
+            var authors = options.volumeInfo.authors ? options.volumeInfo.authors : ['Unknown Author'],
+                thumbnail = options.volumeInfo.imageLinks ? options.volumeInfo.imageLinks.smallThumbnail : '/images/no_image.jpg';
+
+            this.set('authors', authors)
+                .set('thumbnail', thumbnail)
+                .set('title', options.volumeInfo.title)
+                .set('previewLink', options.volumeInfo.previewLink);
         }
     });
 });
